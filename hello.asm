d9726db60e675edf02837d68643c92b37377d87e
mirzacamic <camic.mirza@gmail.com>
2022-05-13 16:36:05 +0200

    improved version of hello.asm

--- hello.asm
+++ hello.asm
@@ -1,22 +1,58 @@
 ; hello.asm
 ; My first x86 assembly programm
 
+;User input and a greeting
+
 ; Author: Mirza Camic
 ; Date: 13.05.2022
 
 section .data
-    text db "Hello World!",10
+    text1 db "Whats your name? "
+    text2 db "Hello, "
+
+section .bss
+    name resb 16
 
 section .text
     global _start
-
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
+    call _printText1
+    call _getName
+    call _printText2
+    call _printName
 
     mov rax, 60
     mov rdi, 0
     syscall
+
+_getName:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, name
+    mov rdx, 16
+    syscall
+    ret
+
+_printText1:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text1
+    mov rdx, 17
+    syscall
+    ret
+
+_printText2:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text2
+    mov rdx, 7
+    syscall
+    ret
+
+_printName:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, name
+    mov rdx, 16
+    syscall
+    ret
